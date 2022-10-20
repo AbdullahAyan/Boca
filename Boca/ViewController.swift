18687b61518b6c4149fc2ac0584e4f7d1ea28e43
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-10-20 14:01:29 +0300

    Initial Commit

---
+++ Boca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Boca
+//
+//  Created by Abdullah Ayan on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
