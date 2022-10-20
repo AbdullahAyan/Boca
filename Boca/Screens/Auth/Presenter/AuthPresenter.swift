67406aca4676878cf4570be1524427f06d896fec
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-10-20 16:13:21 +0300

    VIPER Patern added

---
+++ Boca/Screens/Auth/Presenter/AuthPresenter.swift
@@ -0,0 +1,16 @@
+//
+//  AuthPresenter.swift
+//  Boca
+//
+//  Created by Abdullah Ayan on 20.10.2022.
+//
+
+import Foundation
+
+class AuthPresenter: ViewControllerToPresenterAuthProtocol {
+    
+    var authViewController: ViewToViewControllerAuthProtocol?
+    var authInteractor: PresenterToInteractorAuthProtocol?
+    
+    
+}
---
+++ Boca/Screens/Auth/View/AuthView.swift
@@ -0,0 +1,13 @@
+//
+//  AuthView.swift
+//  Boca
+//
+//  Created by Abdullah Ayan on 20.10.2022.
+//
+
+import UIKit
+
+class AuthView: UIView {
+
+
+}
