456417f52073033e8560f908bb9c2c4d552c5ce7
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-10-25 14:14:21 +0300

    BasketView finished

--- Boca/Screens/Basket/BasketView/BasketView.swift
+++ Boca/Screens/Basket/BasketView/BasketView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BasketView: UIView {
+final class BasketView: UIView {
 
     var basketViewController: BasketViewController?
     
@@ -17,14 +17,33 @@ class BasketView: UIView {
         return tableView
     }()
     
-    private lazy var updateButton: UIButton = {
+    private lazy var orderButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .red
-        button.setTitle("Güncelle", for: .normal)
+        button.setTitle("Sipariş Ver", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 10
         return button
+    }()
+    
+    private lazy var totalPriceLabel: UILabel = {
+        let label = UILabel()
+
+        label.text = "44 ₺"
+        label.font = UIFont(name: "Mukta-Medium", size: 20)
+        label.backgroundColor = .white
+        label.textAlignment = .center
+
+        
+        label.layer.cornerRadius = 10.0
+        label.layer.masksToBounds = true
+
+        label.layer.borderColor = UIColor.lightGray.cgColor
+        label.layer.borderWidth = 1.5
+        
+        
+        return label
     }()
     
 
@@ -34,23 +53,35 @@ class BasketView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        self.backgroundColor = .white
+        
         let nib = UINib(nibName: "BasketTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "cell")
         
-        addSubview(updateButton)
-        updateButton.snp.makeConstraints { make in
+        addSubview(orderButton)
+        orderButton.snp.makeConstraints { make in
             make.left.equalToSuperview().offset(40)
-            make.bottom.right.equalToSuperview().offset(-40)
+            make.bottom.equalToSuperview().offset(-40)
+            make.right.equalToSuperview().offset(-100)
             make.height.equalTo(50)
+        }
+        
+        addSubview(totalPriceLabel)
+        totalPriceLabel.snp.makeConstraints { make in
+            make.left.equalTo(orderButton.snp.right).offset(-35)
+            make.right.equalToSuperview().offset(-30)
+            make.centerY.height.equalTo(orderButton)
         }
         
         addSubview(tableView)
         tableView.snp.makeConstraints { make in
             make.top.left.right.equalToSuperview()
-            make.bottom.equalTo(updateButton.snp.top).offset(-10)
+            make.bottom.equalTo(orderButton.snp.top).offset(-10)
         }
         
-        self.backgroundColor = .white
+
+            
+        
         
 
     }
