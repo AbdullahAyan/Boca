40e0b8f1330ec42eef3562772f07c91b9356484f
Abdullah Ayan <75751013+AbdullahAyan@users.noreply.github.com>
2022-10-26 16:17:21 +0300

    Revert "adding profile feature"

--- Boca/Screens/Auth/Entity/AuthModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AuthModel.swift
-//  Boca
-//
-//  Created by Abdullah Ayan on 20.10.2022.
-//
-
-import Foundation
--- Boca/Screens/Food/FoodEntity/FoodEntity.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FoodEntity.swift
-//  Boca
-//
-//  Created by Abdullah Ayan on 24.10.2022.
-//
-
-import Foundation
